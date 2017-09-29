fe494b19d28996140148e2180167a512082499cb
davemachado <dave@davemachado.com>
2017-09-29 17:44:25 -0400

    Stub out routes

--- main.go
+++ main.go
@@ -1,13 +1,65 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	app := gin.Default()
+
+	users := app.Group("/users")
+	{
+		users.GET("/login", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"page": "login",
+			})
+		})
+		users.GET("/logout", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"page": "logout",
+			})
+		})
+		users.GET("/register", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"page": "register",
+			})
+		})
+	}
+
+	stocks := app.Group("/stocks")
+	{
+		stocks.GET("/:symbol", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"page": fmt.Sprintf("/stocks/%s", c.Param("symbol")),
+			})
+		})
+	}
+
+	portfolio := app.Group("/portfolio")
+	{
+		portfolio.GET("/buy", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"page": fmt.Sprintf("/buy"),
+			})
+		})
+		portfolio.GET("/sell", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"page": fmt.Sprintf("/sell"),
+			})
+		})
+		portfolio.GET("/update", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"page": fmt.Sprintf("/update"),
+			})
+		})
+	}
+
+	app.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	app.Run() // listen and serve on 0.0.0.0:8080
 }
