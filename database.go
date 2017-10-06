e9010783bb564615637e49fc436de64a15d48699
davemachado <dave@davemachado.com>
2017-10-06 15:38:05 -0400

    check  for length

--- database.go
+++ database.go
@@ -14,7 +14,7 @@ func setupDB() {
 	if len(dbUsername) == 0 {
 		panic("$DB_USERNAME is not set")
 	}
-	if len(dbUsername) == 0 {
+	if len(dbPassword) == 0 {
 		panic("$DB_PASSWORD is not set")
 	}
 	db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@/", dbUsername, dbPassword))
